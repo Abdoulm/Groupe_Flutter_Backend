8a14ce07d5f3d6df8c2d41cc147ae4981dbd4bad
Diakalia Konaté <diakalia.konate@odc.local>
2021-11-15 16:00:34 +0000

    Update projet

--- src/main/java/com/groupeflutter/groupeflutterapi/Model/Category.java
+++ src/main/java/com/groupeflutter/groupeflutterapi/Model/Category.java
@@ -18,8 +18,18 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private String nomCategory;
+
+    @Column(nullable = false)
+    private String imageCategory;
+
+    @Column(nullable = false)
+    private String audioCategory;
+
+    @Column(nullable = false)
+    private String descriptionCategory;
 
     @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
     private List<Panneaux> panneaux;
